8092c8666b975506994e86ec385137eec9c3876a
Harpreet Singh <designsbyharp@gmail.com>
2021-04-27 14:56:05 -0400

    Boilerplate set up
    Set up for:
    
    1. index.html
    2. style.css
    3. app.js

---
+++ docs/app.js
@@ -0,0 +1,13 @@
+let draggableSlider = function () {
+  // DOM element(s)
+  const header = document.querySelector("header");
+
+  // variables to use, for storing scroll data
+  let prevScrollPos = 0;
+  let currentScrollPosition;
+
+  // Window.onscroll - call toggleNavbar()
+};
+
+// Invoke code
+draggableSlider();
